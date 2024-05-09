1345ba5ded8a78b08b83b7bdc5f50494b2919e83
Teri P <psegiannakis@outlook.com>
2024-05-09 16:59:12 +1000

    incorporate correct APIs when sport is clicked

--- sports-hub/src/Body.tsx
+++ sports-hub/src/Body.tsx
@@ -2,7 +2,7 @@ export default function Body() {
   return (
     <>
       <div>
-        <h1></h1>
+        <h1>{}</h1>
       </div>
     </>
   );
--- sports-hub/src/Header.tsx
+++ sports-hub/src/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import {
   Carousel,
   CarouselContent,
@@ -10,14 +11,37 @@ import {
 export default function Header() {
   const [selectedSport, setSelectedSport] = useState("");
 
-  // Function to handle the click on a sport item
+  // fix Apis to corrspond to the correct request (fix date so its always current)
+  const nbaApi = `https://v2.nba.api-sports.io/games?date=2022-03-09`;
+  const footballApi = `https://v3.football.api-sports.io/games?date=2022-03-09`;
+  const ufcApi = `https://v1.mma.api-sports.io/games?date=2022-03-09`;
+  const f1Api = `https://v1.formula-1.api-sports.io/games?date=2022-03-09`;
+  const aflApi = `https://v1.afl.api-sports.io/games?date=2022-03-09`;
+
+  // Function to handle the click on a sport
   const handleSportClick = (event: React.MouseEvent<HTMLDivElement>) => {
     const sportId = event.currentTarget.id;
     setSelectedSport(sportId);
-
-    const api = `https://${selectedSport}.api-sports.io/`;
   };
+  console.log(selectedSport);
 
+  const apiSelected = useQuery({
+    queryKey: ["selectedApi", selectedSport],
+    queryFn: () =>
+      fetch(selectedSport, {
+        headers: {
+          "x-rapidapi-key": import.meta.env.VITE_API_KEY,
+        },
+      }).then((res) => res.json()),
+    enabled: !!selectedSport,
+  });
+
+  console.log(apiSelected.data);
+
+  // Render loading state if data is still loading
+
+  // Render fetched data if available
+  // Render fetched data if available
   return (
     <>
       <div className="bg-cyan-900 font-oswald text-white flex flex-col justify-center items-center ">
@@ -25,35 +49,35 @@ export default function Header() {
         <Carousel className="w-2/3 text-xl cursor-pointer ">
           <CarouselContent className="ml-28">
             <CarouselItem
-              id="v2.nba"
+              id={nbaApi}
               className="basis-1/3"
               onClick={handleSportClick}
             >
               🏀 NBA 🏀
             </CarouselItem>
             <CarouselItem
-              id="v3.football"
+              id={footballApi}
               className="basis-1/3"
               onClick={handleSportClick}
             >
               ⚽️ FOOTBALL ⚽️
             </CarouselItem>
             <CarouselItem
-              id="v1.mma"
+              id={ufcApi}
               className="basis-1/3"
               onClick={handleSportClick}
             >
               🥊 UFC 🥊
             </CarouselItem>
             <CarouselItem
-              id="v1.formula-1"
+              id={f1Api}
               className="basis-1/3"
               onClick={handleSportClick}
             >
               🏁 F1 🏁
             </CarouselItem>
             <CarouselItem
-              id="v1.afl"
+              id={aflApi}
               className="basis-1/3"
               onClick={handleSportClick}
             >
@@ -63,6 +87,15 @@ export default function Header() {
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
+      </div>
+      <div>
+        {/* Check if data is available before rendering */}
+        {apiSelected.data && (
+          <>
+            <h1>Response: {apiSelected.data.response[0].teams.home.name}</h1>
+            {/* Render other properties as needed */}
+          </>
+        )}
       </div>
     </>
   );
